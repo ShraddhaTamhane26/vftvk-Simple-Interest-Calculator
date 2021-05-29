9bf1f11c8656f0667dabe72d88b22d9eae1e0597
Shraddha Tamhane <82407272+ShraddhaTamhane26@users.noreply.github.com>
2021-05-29 23:10:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-####<script>
-    function compute()
-    {
-        let p=document.getElementById('amount').value;
-        let n=document.getElementById('year').value;
-        let r=document.getElementById('interest rate').value;
-
-        let  SI=(p*n*r/100);
-
-        document.getElementById('si').innerHTML="You will receive an amount of: "+SI;
-    }
-</script>
+function compute()
+{
+    p = document.getElementById("principal").value;
+    
+}
