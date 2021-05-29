bdd03917426df16f39ede96d1a3867d15b398842
Shraddha Tamhane <82407272+ShraddhaTamhane26@users.noreply.github.com>
2021-05-29 21:11:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ function compute()
                 
                 result.innerHTML = "The interest is " + (p*n*r/100);
     
+    function compute()
+    {
+        let p=document.getElementById('amount').value;
+        let n=document.getElementById('years').value;
+        let r=document.getElementById('interest rate').value;
+
+        let  SI=(p*n*r)/100;
+
+        document.getElementById('si').innerHTML="You will receive an amount of: "+SI;
+    }
+    
 }
         
