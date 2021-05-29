8f8041669ddf5558cbc564b0963c683009d60cd9
Shraddha Tamhane <82407272+ShraddhaTamhane26@users.noreply.github.com>
2021-05-29 20:38:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    p = document.getElementById("p").value;
+    n = document.getElementById("n").value;
+    r = document.getElementById("r").value;
+    result = document.getElementById("result");
+                
+                result.innerHTML = "The interest is " + (p*n*r/100);
     
 }
         
